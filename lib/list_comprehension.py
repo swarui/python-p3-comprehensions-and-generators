110e21ef7aff84fd8aaa317df7d1cd4d91d99e9a
swarui <stevewaruim@gmail.com>
2023-11-24 08:39:12 +0300

    Completed Assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    return [even for even in num_list if even % 2 ==0]
 
 def make_exclamation(sentence_list):
-    pass
+    return [f"{string}!" for string in sentence_list]
